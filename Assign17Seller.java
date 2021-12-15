91283917d861d90090deb532b1256bed1ed74722
helunahab <95560080+helunahab@users.noreply.github.com>
2021-12-14 23:44:40 -0600

    Add files via upload

--- Assign17Seller.java
+++ Assign17Seller.java
@@ -4,26 +4,29 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
 public class Assign17Seller {
 
 	public static void main(String[] args) {
+		
+		
 		try {
 			
 			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Assig17ecommdb", "root", "root");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Assig17", "root", "root");
 			Statement stmt = con.createStatement();
-	         ResultSet rs = stmt.executeQuery(("select ItemName,Price,Quantity,User_Id from Product")); 
-	         
-	         while (rs.next()) {
-	            
-	            System.out.print("ItemName: " + rs.getString("itemName"));
-	            System.out.print(", Price: " + rs.getInt("price"));
-	            System.out.print(", Quantity: " + rs.getInt("quantity"));
-	            System.out.println("User_Id: " + rs.getString("userid"));
-	          }
-	       
-			}catch (SQLException e) {
+	        ResultSet rs = stmt.executeQuery("select ItemName,Price,Quantity,Pro_ID from Product"); 
+	        System.out.println("ItemName"+"\t"+"Price"+"\t"+"Quantity"+"\t"+"Pro_ID");
+	        while (rs.next()) {
+	         System.out.print( rs.getString("itemName")+"\t");//+"\t"+rs.getInt("price")+"\t"+rs.getInt("quantity")+"\t"+rs.getInt("Pro_ID")+"\n"); 
+	            System.out.print( rs.getInt("price")+"\t");
+	            System.out.print( rs.getInt("quantity")+"\t");
+	            System.out.print( rs.getInt("Pro_ID")+"\t" +"\n");
+		   }
+	         	con.close();
+				}catch (SQLException e) {
 	         	e.printStackTrace();
-	      } 
+	
+			} 
 	}
 }
